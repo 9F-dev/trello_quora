a5f89d48e12bdca8b9b68be4d0d85792b542741f
A Anasuya Threse Innocent <55600315+Anasuya-Smith@users.noreply.github.com>
2020-01-12 16:17:22 +0530

    Merge branch 'master' of https://github.com/rinsheed/trello_quora into anasuya-smith
    # Conflicts:
    #	quora-api/target/classes/com/upgrad/quora/api/controller/UserController.class
    #	quora-api/target/classes/com/upgrad/quora/api/model/ErrorResponse.class
    #	quora-api/target/classes/com/upgrad/quora/api/model/SigninResponse.class
    #	quora-api/target/classes/com/upgrad/quora/api/model/SignoutResponse.class
    #	quora-api/target/classes/com/upgrad/quora/api/model/SignupUserRequest.class
    #	quora-api/target/classes/com/upgrad/quora/api/model/SignupUserResponse.class
    #	quora-api/target/generated-sources/com/upgrad/quora/api/model/ErrorResponse.java
    #	quora-api/target/generated-sources/com/upgrad/quora/api/model/SigninResponse.java
    #	quora-api/target/generated-sources/com/upgrad/quora/api/model/SignoutResponse.java
    #	quora-api/target/generated-sources/com/upgrad/quora/api/model/SignupUserRequest.java
    #	quora-api/target/generated-sources/com/upgrad/quora/api/model/SignupUserResponse.java
    #	quora-service/src/main/java/com/upgrad/quora/service/business/UserSignupService.java
    #	quora-service/src/main/java/com/upgrad/quora/service/entity/AnswerEntity.java
    #	quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
    #	quora-service/target/classes/com/upgrad/quora/service/business/JwtTokenProvider.class
    #	quora-service/target/classes/com/upgrad/quora/service/business/SignoutBusinessService.class
    #	quora-service/target/classes/com/upgrad/quora/service/business/SignupBusinessService.class
    #	quora-service/target/classes/com/upgrad/quora/service/dao/UserDAO.class
    #	quora-service/target/classes/com/upgrad/quora/service/entity/UserAuthEntity.class

--- quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -1,6 +1,5 @@
 package com.upgrad.quora.service.entity;
 
-<<<<<<< HEAD
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -18,50 +17,25 @@ import java.util.Objects;
 
 })
 public class QuestionEntity implements Serializable {
-
-=======
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
-import javax.persistence.*;
-import javax.validation.constraints.Size;
-import java.io.Serializable;
-
-@Entity
-@Table(name = "question", schema = "quora")
-@NamedQueries(
-        value = {
-                @NamedQuery(name = "questionByUuid", query = "select q from AnswerEntity q where q.uuid = :uuid"),
-        }
-)
-
-public class QuestionEntity implements Serializable {
-
-    // Columns are: id, uuid, content, date, userId
-
->>>>>>> 49946125b824dc26dfc903a7114bd494890f5e83
     @Id
-    @Column(name = "ID")
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "UUID")
-    @Size(max = 200)
+    @Column(name = "uuid")
+    @Size(max = 64)
     private String uuid;
 
-    @Column(name = "CONTENT")
+    @Column(name = "content")
     @Size(max = 500)
-<<<<<<< HEAD
     private String content;
 
-    @Column(name = "DATE")
+    @Column(name = "date")
     @NotNull
     private ZonedDateTime date;
 
     @ManyToOne
-    @JoinColumn(name = "USER_ID")
+    @JoinColumn(name = "user_id")
     private UserEntity user;
 
     @Override
@@ -79,27 +53,6 @@ public class QuestionEntity implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getUuid(), getContent(), getDate(), getUser());
-=======
-    private AnswerEntity content;
-
-    @Column(name = "DATE")
-    private String date;
-
-    //Relationship with other entity
-
-    @ManyToOne
-    @Column(name = "USER_ID")
-    private UserEntity userId;
-
-    @OneToMany
-    @Column(name = "ANSWER_ID")
-    private AnswerEntity answerId;
-
-
-    @Override
-    public boolean equals(Object obj) {
-        return new EqualsBuilder().append(this, obj).isEquals();
->>>>>>> 49946125b824dc26dfc903a7114bd494890f5e83
     }
 
     public Integer getId() {
@@ -110,25 +63,6 @@ public class QuestionEntity implements Serializable {
         this.id = id;
     }
 
-<<<<<<< HEAD
-=======
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public UserEntity getUserId() {
-        return userId;
-    }
-
-    public void setUserId(UserEntity userId) {
-        this.userId = userId;
-    }
-
->>>>>>> 49946125b824dc26dfc903a7114bd494890f5e83
     public String getUuid() {
         return uuid;
     }
@@ -137,7 +71,6 @@ public class QuestionEntity implements Serializable {
         this.uuid = uuid;
     }
 
-<<<<<<< HEAD
     public String getContent() {
         return content;
     }
@@ -163,33 +96,3 @@ public class QuestionEntity implements Serializable {
     }
 
 }
-
-=======
-    public AnswerEntity getContent() {
-        return content;
-    }
-
-    public void setContent(AnswerEntity content) {
-        this.content = content;
-    }
-
-    public AnswerEntity getAnswerId() {
-        return answerId;
-    }
-
-    public void setAnswerId(AnswerEntity answerId) {
-        this.answerId = answerId;
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder().append(this).hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
-    }
-
-}
->>>>>>> 49946125b824dc26dfc903a7114bd494890f5e83
